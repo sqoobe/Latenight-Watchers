217b4fc06d0957c86eb8bdf8612d3d9945205965
sqoobe <sqoobe96@gmail.com>
2025-03-27 14:22:42 +0100

    start

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import SearchMovies from '../components/searchMovies.jsx'
+
+function App() {
+
+  return (
+    <div className="container">
+      <h1 className="title">React Movie Search</h1>
+      <SearchMovies />
+    </div>
+  )
+}
+
+export default App
