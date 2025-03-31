afcebb38e753756a408c723b4df44e3a492da3d5
sqoobe <sqoobe96@gmail.com>
2025-03-31 12:14:03 +0200

    added comments to code

--- components/movieCard.jsx
+++ components/movieCard.jsx
@@ -1,19 +1,37 @@
 import React from "react"
 
-export default function MovieCard({movie}){
+/**
+ * MovieCard - Komponent for å vise informasjon om en film
+ * 
+ * Denne komponenten tar imot et movie-objekt som prop og viser 
+ * filmens poster, tittel, utgivelsesdato, vurdering og beskrivelse
+ * i et kort-format.
+ * 
+ * @param {Object} props - Komponentens props
+ * @param {Object} props.movie - Filmobjekt med informasjon fra TMDB API
+ * @returns {JSX.Element} - Rendret filmkort
+ */
+export default function MovieCard({movie}) {
     return (
-         <div className="card" >
+        <div className="card">
+            {/* Filmposter fra TMDB API med w185_and_h278_bestv2 størrelse */}
             <img className="card--image"
                 src={`https://image.tmdb.org/t/p/w185_and_h278_bestv2/${movie.poster_path}`}
-                alt={movie.title + ' poster'}
-                />
+                alt={movie.title + ' poster'} // Tilgjengelig alt-tekst for skjermlesere
+            />
             <div className="card--content">
-            <h3 className="card--title">{movie.title}</h3>
-            <p><small>RELEASE DATE: {movie.release_date}</small></p>
-            <p><small>RATING: {movie.vote_average}</small></p>
-            <p className="card--desc">{movie.overview}</p>
+                {/* Filmtittel */}
+                <h3 className="card--title">{movie.title}</h3>
+                
+                {/* Informasjon om utgivelsesdato */}
+                <p><small>RELEASE DATE: {movie.release_date}</small></p>
+                
+                {/* Filmens vurdering basert på stemmer */}
+                <p><small>RATING: {movie.vote_average}</small></p>
+                
+                {/* Filmbeskrivelse/sammendrag */}
+                <p className="card--desc">{movie.overview}</p>
             </div>
-
         </div>
     )
 }
--- components/searchMovies.jsx
+++ components/searchMovies.jsx
@@ -1,31 +1,50 @@
 import React, {useState} from "react";
 import MovieCard from "./movieCard";
 
+/**
+ * SearchMovies - Komponent for å søke etter filmer
+ * 
+ * Denne komponenten lar brukeren søke etter filmer ved hjelp av TMDB API.
+ * Den håndterer både input fra brukeren, API-forespørsler og visning av
+ * søkeresultater i form av MovieCard-komponenter.
+ * 
+ * @returns {JSX.Element} - Rendret søkeskjema og liste med filmkort
+ */
 export default function SearchMovies() {
 
-  //states- input query, movies
-  const [query, setQuery] = useState ('');
-  //create the state for movies, and update that state appropiate
+  // State for søketekst fra bruker
+  const [query, setQuery] = useState('');
+  
+  // State for filmresultater fra API
   const [movies, setMovies] = useState([]);
   
+  /**
+   * Funksjon for å søke etter filmer via TMDB API
+   * 
+   * @param {Event} e - Submit-hendelsen fra skjemaet
+   */
   const searchMovies = async (e) => {
-    e.preventDefault();//prevent from doing its default action of posting its data in a query
-
+    e.preventDefault(); // Forhindrer standardoppførsel med sideoppdatering
+    
+    // URL for TMDB API med søketekst fra brukeren
     const url = `https://api.themoviedb.org/3/search/movie?api_key=37c0f7cf8ebd0ea898354c7c6ce17fc7&language=en-US&query=${query}&page=1&include_adult=false`;
 
     try {
-      const res = await fetch(url) 
-      const data = await res.json()
-      setMovies(data.results)
-    
-  }catch(err){
-    console.error(err);
+      // Utfør API-forespørsel
+      const res = await fetch(url);
+      // Konverter respons til JSON
+      const data = await res.json();
+      // Oppdater movies state med resultatene
+      setMovies(data.results);
+    } catch(err) {
+      // Håndter feil under API-forespørselen
+      console.error(err);
+    }
   }
-}
-
 
   return (
   <>
+    {/* Søkeskjema */}
     <form className="form" onSubmit={searchMovies}>
       <label className="label" htmlFor="query">
         Movie Name
@@ -35,16 +54,20 @@ export default function SearchMovies() {
         type="text"
         name="query"
         placeholder="i.e. Jurassic Park"
-        value={query} onChange={(e) => setQuery(e.target.value)}
+        value={query} 
+        onChange={(e) => setQuery(e.target.value)} // Oppdater query state ved endring
       />
       <button className="button" type="submit">
         Search
       </button>
     </form>
+    
+    {/* Liste med filmkort */}
     <div className="card-list">
+        {/* Filtrer ut filmer uten poster og map hver film til en MovieCard-komponent */}
         {movies.filter(movie => movie.poster_path).map(movie => (
           <MovieCard movie={movie} key={movie.id} />
-       ))}
+        ))}
     </div>    
   </>
   );
